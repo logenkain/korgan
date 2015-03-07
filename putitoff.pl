5cbee89ead484f3434a3aaaff842fc5bf5f01500
Logen Kain <walach.of.harkon@gmail.com>
2015-03-07 17:26:05 -0600

    Rewrote as subs

--- putitoff.pl
+++ putitoff.pl
@@ -5,41 +5,52 @@ use strict;
 
 use File::Path qw(mkpath);
 
-
 package main;
 
-#Check if .local/putitoff directory exists
-if ( -d "$ENV{HOME}/.local/putitoff") {
-  print "dir exists\n";
-}
-else{
-  print  "dir doesn't exist\n";
-  print  "creating directory\n";
-  mkpath("$ENV{HOME}/.local/putitoff");
+#######SUBROUTINES##########
+sub init_dir {
+	my $dir = shift;
 
+	if ( -d "$dir") {
+  	print "dir exists\n";
+	}
+	else{
+ 	 print  "dir doesn't exist\n";
+ 	 print  "creating directory\n";
+ 	 mkpath("$ENV{HOME}/.local/putitoff");
+	}
 }
+
+#If the file is there print it out, else create a test case and print it out
+sub init_file {
+my $fh;
+my $filename = shift;
+	if (open($fh, '<', $filename)) {
+	  print "file exists\n";
+ 	 print <$fh>;
+ 	 close $fh;
+	}
+	else {
+  	print "Creating example file\n";  
+  	open($fh, '>', $filename) or die "Can't write to $filename"; 
+  	print $fh "Test 1\nTest 2\nTest 3\n";
+  	close $fh;
+  	
+		open($fh, '<', $filename); 
+  	print <$fh>;
+  	close $fh;
+	}
+}
+
+#######END SECTION##########
 
 #assign our database file to $filename REMEMBER ~/ DOES NOT WORK
 my $filename = "$ENV{HOME}/.local/putitoff/putitoff.db";
 
-#localize the filehandler
-my $fh;
+#Local dir to save database
+my $dir = "$ENV{HOME}/.local/putitoff/";
 
-#If the file is there print it out, else create a test case and print it out
-if (open($fh, '<', $filename)) {
-  print "file exists\n";
-  print <$fh>;
-  close $fh;
-}
-else {
-  print "Creating example file\n";  
-  open($fh, '>', $filename) or die "fuck"; 
-  print $fh "Test 1\nTest 2\nTest 3\n";
-  close $fh;
+#initalize directory and file if they do not exist.
+init_dir($dir);
+init_file($filename);
 
-
-  open($fh, '<', $filename); 
-  print <$fh>;
-  close $fh;
-
-}
